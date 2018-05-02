9b6a1c9692b3e5e77078af48f2e4ee63b8084067
Chalk <chalk@chalk.pe>
2018-05-02 23:52:15 +0900

    remove double check

--- src/logger.js
+++ src/logger.js
@@ -1,13 +1,10 @@
 module.exports = {
   enqueue: (item, child) =>
-    (process.env.QUIET_ENQUEUE !== '1')
-    && console.log('enqueue', item.title, '->', child),
+    console.log('enqueue', item.title, '->', child),
 
   dequeue: item =>
-    (process.env.QUIET_DEQUEUE !== '1')
-    && console.log('dequeue', item.title),
+    console.log('dequeue', item.title),
 
   status: (queue, visited) =>
-    (process.env.QUIET_STATUS !== '1')
-    && console.log('status: queue', queue.length, 'visited', visited.size)
+    console.log('status: queue', queue.length, 'visited', visited.size)
 }
